d5244aa5afe4a28616ed13a85f8bf14df4786188
Joaquin Alonso <jalonso1@miuandes.cl>
2019-04-18 18:16:48 -0400

    Lab Finalizado

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,8 @@ class SessionsController < ApplicationController
 	end
 
 	def create
-		if user = User.where(email: params[:session][:email],password: params[:session][:password])[0]
+		user = User.where(email: params[:session][:email],password: params[:session][:password])[0]
+		if user != nil
       session[:current_user_id] = user.id
 			cookies["log in"] = String(user.id)
       redirect_to "/users/" +String(user.id)
@@ -16,6 +17,7 @@ class SessionsController < ApplicationController
 
 	def destroy
 		session[:current_user_id] = nil
+		cookies["log in"] = nil
     flash[:notice] = "You have successfully logged out."
     redirect_to root_path
 		#complete this method
